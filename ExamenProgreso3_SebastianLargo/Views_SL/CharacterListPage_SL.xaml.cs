3aef099e127b30c045ec547257ea875b8881594d
mango2309 <sebastian.largo@udla.edu.ec>
2024-07-10 11:48:08 -0500

    Se creo la vista de la lista de personajes en la carpetas Views_SL

---
+++ ExamenProgreso3_SebastianLargo/Views_SL/CharacterListPage_SL.xaml.cs
@@ -0,0 +1,9 @@
+namespace ExamenProgreso3_SebastianLargo.Views_SL;
+
+public partial class CharacterListPage_SL : ContentView
+{
+	public CharacterListPage_SL()
+	{
+		InitializeComponent();
+	}
+}
--- ExamenProgreso3_SebastianLargo/Views_SL/Character_SL.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace ExamenProgreso3_SebastianLargo.Views_SL;
-
-public partial class NewContent1 : ContentView
-{
-	public NewContent1()
-	{
-		InitializeComponent();
-	}
-}
